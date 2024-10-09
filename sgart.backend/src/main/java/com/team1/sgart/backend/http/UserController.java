afc6cc851214b10e43478d639a6f5b24d22c80a7
KarManiatic <Carlos.Romero16@alu.uclm.es>
2024-10-09 12:35:38 +0200

    TDD para User Controller.

--- sgart.backend/src/main/java/com/team1/sgart/backend/http/UserController.java
+++ sgart.backend/src/main/java/com/team1/sgart/backend/http/UserController.java
@@ -1,29 +1,35 @@
 package com.team1.sgart.backend.http;
 
+import java.util.Map;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.team1.sgart.backend.model.User;
 import com.team1.sgart.backend.services.UserService;
 
 @RestController
 @RequestMapping("users")
 @CrossOrigin("*")
 public class UserController {
-	
-	@Autowired
-	private UserService userService;
-	
-	@PostMapping("/registro")
-	public void registrarUser() {
-		userService.registrarUser(null);
-	}
-	
-	//Aquí haremos los comprobadores de formato y tratamiento de entrada desde el Frontend
-	public void comprobarPassword() {
-		//Comprobador de dualidad de contraseña
-		//Comprobador de formato de contraseña
-	}
+
+    @Autowired
+    private UserService userService;
+
+    @PostMapping("/registro")
+    public ResponseEntity<String> registrar(@RequestBody @Validated Map<String, Object> user) {
+
+        if (userService.emailYaRegistrado(((User) user).getEmail())) {
+            return ResponseEntity.status(HttpStatus.CONFLICT).body("Email already in use");
+        }
+        userService.registrarUser(user);
+        return ResponseEntity.status(HttpStatus.CREATED).body("User registered successfully");
+    }
 }
--- sgart.backend/src/test/java/com/team1/sgart/backend/http/UserControllerTest.java
+++ sgart.backend/src/test/java/com/team1/sgart/backend/http/UserControllerTest.java
@@ -1,6 +1,96 @@
 package com.team1.sgart.backend.http;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.team1.sgart.backend.model.User;
+import com.team1.sgart.backend.services.UserService;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
+
+import static org.mockito.Mockito.when;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+
+@WebMvcTest(UserController.class)
 public class UserControllerTest {
-	
-	
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @MockBean
+    private UserService userService;
+    
+
+    User user;
+    @BeforeEach
+    public void setUp() {
+        user = new User("Carlos", "Romero Navarro", "Quality", "Ciudad Real", "carlos.romero@example.com", "01/01/2024", 
+                        "Scrum Developer", "password123@", "password123@", true, false);
+    }
+
+    @Test
+    public void usuarioValido_devuelve201() throws Exception {
+        ObjectMapper objectMapper = new ObjectMapper();
+        String userJson = objectMapper.writeValueAsString(user);
+
+        mockMvc.perform(post("/users/registro")
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(userJson))
+                .andExpect(status().isCreated())
+                .andExpect(content().string("Usuario registrado correctamente"));
+    }
+
+    @Test
+    public void emailUsuarioInvalido_devuelve400() throws Exception {
+        
+        ObjectMapper objectMapper = new ObjectMapper();
+        String userJson = objectMapper.writeValueAsString(user);
+
+        mockMvc.perform(post("/users/registro")
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(userJson))
+                .andExpect(status().isBadRequest())
+                .andExpect(jsonPath("$.errors.email").value("Formato del email incorrecto"));
+    }
+
+    @Test
+    public void usuarioExistente_devuelve409() throws Exception {
+       when(userService.emailYaRegistrado(user.getEmail())).thenReturn(true);
+
+        ObjectMapper objectMapper = new ObjectMapper();
+        String userJson = objectMapper.writeValueAsString(user);
+
+        mockMvc.perform(post("/users/registro")
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(userJson))
+                .andExpect(status().isConflict())
+                .andExpect(content().string("Email en uso")); //Sujeto a cambios de seguridad
+    }
+
+    @Test
+    public void passwordDebil_devuelve400() throws Exception {
+        ObjectMapper objectMapper = new ObjectMapper();
+        String userJson = objectMapper.writeValueAsString(user);
+
+        mockMvc.perform(post("/users/register")
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(userJson))
+                .andExpect(status().isBadRequest())
+                .andExpect(jsonPath("$.errors.password").value("Siga las instrucciones de seguridad para la contraseña"));
+    }
+
+    @Test
+    public void jsonMalformado_devuelve400() throws Exception {
+        String malformedJson = "{ \"name\": \"John\", \"lastName\": \"Doe\" "; // JSON malformado
+
+        mockMvc.perform(post("/users/register")
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(malformedJson))
+                .andExpect(status().isBadRequest());
+    }
 }
