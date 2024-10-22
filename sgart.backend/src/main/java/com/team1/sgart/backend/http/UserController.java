6f0eecb044c75d2484fcbec16bbb46d1ce7a07c2
KarManiatic <Carlos.Romero16@alu.uclm.es>
2024-10-22 17:02:27 +0200

    Cambios en Modificar Usuarios

--- sgart.backend/src/main/java/com/team1/sgart/backend/http/UserController.java
+++ sgart.backend/src/main/java/com/team1/sgart/backend/http/UserController.java
@@ -26,4 +26,11 @@ public class UserController {
         userService.registrarUser(user);
         return ResponseEntity.status(HttpStatus.CREATED).body("Usuario registrado correctamente");        
     }
+    
+    @PostMapping("/modificar")
+	public ResponseEntity<String> modificar(@RequestBody User user) {
+
+		userService.modificarUser(user);
+		return ResponseEntity.status(HttpStatus.OK).body("Usuario modificado correctamente");
+	}
 }
