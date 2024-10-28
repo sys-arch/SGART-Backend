d5b41c37ca85d68918fcbb8fac3ad8b6f49f7c5d
Rosa María <115668275+RosaMariaFdez@users.noreply.github.com>
2024-10-28 10:40:52 +0100

    Añadidos test de eliminar usuario
    hay por email o por id, lo que venga mejor luego desde el front

--- sgart.backend/src/test/java/com/team1/sgart/backend/services/AdminServiceTest.java
+++ sgart.backend/src/test/java/com/team1/sgart/backend/services/AdminServiceTest.java
@@ -10,72 +10,112 @@ import org.mockito.Mockito;
 import org.mockito.junit.jupiter.MockitoExtension;
 
 import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.mockito.Mockito.lenient;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
-import com.team1.sgart.backend.dao.UserDAO;
+import com.team1.sgart.backend.dao.UserDao;
 import com.team1.sgart.backend.model.User;
 
 @ExtendWith(MockitoExtension.class)
 public class AdminServiceTest {
 
-    @Mock
-    private UserDAO userDAO;
+	@Mock
+	private UserDao userDAO;
 
-    @InjectMocks
-    private AdminService adminService;
+	@InjectMocks
+	private AdminService adminService;
 
-    @Test
-    public void testValidarUsuario_UserNotFound() {
-        // Simular que el usuario no existe
-        String userEmail = "user@dominio.com";
-        Mockito.when(userDAO.findByEmail(userEmail))
-                .thenReturn(Optional.empty());
+	@Test
+	public void testValidarUsuario_UserNotFound() {
+		// Simular que el usuario no existe
+		String userEmail = "user@dominio.com";
+		Mockito.when(userDAO.findByEmail(userEmail)).thenReturn(Optional.empty());
 
-        // Ejecutar el test y esperar una excepción
-        Exception exception = assertThrows(IllegalArgumentException.class, () -> {
-            adminService.validarUsuario(userEmail);
-        });
+		// Ejecutar el test y esperar una excepción
+		Exception exception = assertThrows(IllegalArgumentException.class, () -> {
+			adminService.validarUsuario(userEmail);
+		});
 
-        // Verificar el mensaje de la excepción
-        assertEquals("Usuario no encontrado", exception.getMessage());
-    }
+		// Verificar el mensaje de la excepción
+		assertEquals("Usuario no encontrado", exception.getMessage());
+	}
 
-    @Test
-    public void testValidarUsuario_UsuarioYaValidado() {
-        // Simular que el usuario existe y ya está validado
-        String userEmail = "user@dominio.com";
-        User user = new User();
-        user.setEmail(userEmail);
-        Mockito.when(userDAO.findByEmail(userEmail))
-                .thenReturn(Optional.of(user));
-        Mockito.when(userDAO.isUsuarioValidado(userEmail))
-                .thenReturn(true);
+	@Test
+	public void testValidarUsuario_UsuarioYaValidado() {
+		// Simular que el usuario existe y ya está validado
+		String userEmail = "user@dominio.com";
+		User user = new User();
+		user.setEmail(userEmail);
+		Mockito.when(userDAO.findByEmail(userEmail)).thenReturn(Optional.of(user));
+		Mockito.when(userDAO.isUsuarioValidado(userEmail)).thenReturn(true);
 
-        // Ejecutar el test y esperar una excepción
-        Exception exception = assertThrows(IllegalArgumentException.class, () -> {
-            adminService.validarUsuario(userEmail);
-        });
+		// Ejecutar el test y esperar una excepción
+		Exception exception = assertThrows(IllegalArgumentException.class, () -> {
+			adminService.validarUsuario(userEmail);
+		});
 
-        // Verificar el mensaje de la excepción
-        assertEquals("El usuario ya está validado", exception.getMessage());
-    }
+		// Verificar el mensaje de la excepción
+		assertEquals("El usuario ya está validado", exception.getMessage());
+	}
 
-    @Test
-    public void testValidarUsuario_Exito() {
-        // Simular que el usuario existe y no está validado
-        String userEmail = "user@dominio.com";
-        User user = new User();
-        user.setEmail(userEmail);
-        Mockito.when(userDAO.findByEmail(userEmail))
-                .thenReturn(Optional.of(user));
-        Mockito.when(userDAO.isUsuarioValidado(userEmail))
-                .thenReturn(false);
+	@Test
+	public void testValidarUsuario_Exito() {
+		// Simular que el usuario existe y no está validado
+		String userEmail = "user@dominio.com";
+		User user = new User();
+		user.setEmail(userEmail);
+		Mockito.when(userDAO.findByEmail(userEmail)).thenReturn(Optional.of(user));
+		Mockito.when(userDAO.isUsuarioValidado(userEmail)).thenReturn(false);
 
-        // Ejecutar el método
-        adminService.validarUsuario(userEmail);
+		// Ejecutar el método
+		adminService.validarUsuario(userEmail);
 
-        // Verificar que el método de validación fue llamado
-        Mockito.verify(userDAO, Mockito.times(1)).validarUsuario(userEmail);
-    }
+		// Verificar que el método de validación fue llamado
+		Mockito.verify(userDAO, Mockito.times(1)).validarUsuario(userEmail);
+	}
+
+	@Test
+	public void testEliminarUsuarioPorId_UsuarioExiste() {
+		Integer userId = 1;
+		lenient().when(userDAO.findById(userId)).thenReturn(Optional.of(new User()));
+
+		adminService.eliminarUsuarioPorId(userId);
+
+		verify(userDAO, times(1)).deleteById(userId);
+	}
+
+	/*
+	 * EN FRONT SE PASA ID O MAIL QUE SÍ EXISTE
+	 * 
+	 * @Test public void testEliminarUsuarioPorId_UsuarioNoExiste() { Integer userId
+	 * = 999; lenient().when(userDAO.findById(userId)).thenReturn(Optional.empty());
+	 * 
+	 * adminService.eliminarUsuarioPorId(userId);
+	 * 
+	 * verify(userDAO, times(0)).deleteById(userId); }
+	 */
+
+	/*
+	 * @Test public void testEliminarUsuarioPorEmail_UsuarioNoExiste() { String
+	 * userEmail = "nonexistent@example.com";
+	 * lenient().when(userDAO.findByEmail(userEmail)).thenReturn(Optional.empty());
+	 * adminService.eliminarUsuarioPorEmail(userEmail); verify(userDAO,
+	 * times(0)).deleteByEmail(userEmail); }
+	 */
+
+	@Test
+	public void testEliminarUsuarioPorEmail_UsuarioExiste() {
+		String userEmail = "test@example.com";
+		lenient().when(userDAO.findByEmail(userEmail)).thenReturn(Optional.of(new User()));
+
+		adminService.eliminarUsuarioPorEmail(userEmail);
+
+		verify(userDAO, times(1)).deleteByEmail(userEmail);
+	}
+
 }
-
