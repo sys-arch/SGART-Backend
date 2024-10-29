740bbe246d87bb4524ddbb0ff3704d385d8b700a
KarManiatic <Carlos.Romero16@alu.uclm.es>
2024-10-30 00:34:29 +0100

    Añadido CrossOrigin a controlador de doble factor

--- sgart.backend/src/main/java/com/team1/sgart/backend/http/TwoFactorAuthController.java
+++ sgart.backend/src/main/java/com/team1/sgart/backend/http/TwoFactorAuthController.java
@@ -6,6 +6,7 @@ import java.util.concurrent.ConcurrentHashMap;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -18,6 +19,7 @@ import com.team1.sgart.backend.services.TwoFactorAuthService;
 
 @RestController
 @RequestMapping("/auth")
+@CrossOrigin("*")
 public class TwoFactorAuthController {
 
     @Autowired
