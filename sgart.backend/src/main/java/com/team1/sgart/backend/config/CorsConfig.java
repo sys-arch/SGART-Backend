8345aa6070a50c876fb888e9b517cc20f4871eac
AlbertoCazallasMonje <Alberto.Cazallas@alu.ulcm.es>
2024-11-24 18:44:15 +0100

    configuración de cookies

--- sgart.backend/src/main/java/com/team1/sgart/backend/config/CorsConfig.java
+++ sgart.backend/src/main/java/com/team1/sgart/backend/config/CorsConfig.java
@@ -11,7 +11,7 @@ public class CorsConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(@NonNull CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("https://sgart-v1.web.app/")
+                .allowedOrigins("https://sgart-v1.web.app")
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                 .allowedHeaders("*")
                 .allowCredentials(true)
